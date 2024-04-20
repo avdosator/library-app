20b2112172b0682eeeb49350c2cd758177a0af14
Avdo Sator <avdo.sator@hotmail.com>
2024-04-20 20:24:10 +0200

    Add more styles and center elements in place

--- frontend/react-library/src/layouts/HomePage/ExploreTopBooks.tsx
+++ frontend/react-library/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -1,8 +1,8 @@
 
 export const ExploreTopBooks = () => {
     return (
-        <div className="header">
-            <div className="container-fluid">
+        <div className="header p-5 bg-dark mb-4">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
                 <div>
                     <h1 className="display-5 fw-bold">Find your next adventure</h1>
                     <p className="col-md-8 fs-4">Where would you like to go next?</p>
