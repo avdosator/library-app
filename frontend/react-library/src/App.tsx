b6df2330ad18f0ea50886910a6b42ca02e64d92f
Avdo Sator <avdo.sator@hotmail.com>
2024-04-20 15:56:12 +0200

    Create a skeleton of navbar

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,9 +3,17 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Our library app</h1>
-    </>
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>Library App</span>
+        <button className='navbar-toggler' type='button' data-bs-toggle='collapse'
+          data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown'
+          aria-expanded='false' aria-label='Toggle Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+      </div>
+    </nav>
   )
 }
 
