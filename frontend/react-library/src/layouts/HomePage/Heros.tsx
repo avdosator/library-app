6d38174291102bc2e9bcd551911d9b1f2b9db309
Avdo Sator <avdo.sator@hotmail.com>
2024-04-21 11:38:32 +0200

    Create first part of Heros component

---
+++ frontend/react-library/src/layouts/HomePage/Heros.tsx
@@ -0,0 +1,22 @@
+export const Heros = () => {
+    return (
+        <div>
+            <div className="d-none d-lg-block">
+                <div className="row g-0 mt-5">
+                    <div className="col-image-left"></div>
+                </div>
+                <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+                    <div className="ml-2">
+                        <h1>What have you been reading?</h1>
+                        <p className="lead">
+                            The library team would love to know what you have been reading.
+                            Whether it is to learn a new skill or grow within one,
+                            we will be able to provide the top content for you!
+                        </p>
+                        <a href="#" className="btn main-color btn-lg text-white">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
