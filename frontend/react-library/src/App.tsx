8d9468d37f011f03cbd9cb8f8f145151818283c3
Avdo Sator <avdo.sator@hotmail.com>
2024-04-22 10:36:56 +0200

    Make App.tsx arrow function

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,23 +1,14 @@
 import './App.css'
-import { Carousel } from './layouts/HomePage/components/Carousel'
-import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks'
-import { Heros } from './layouts/HomePage/components/Heros';
-import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
+import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar'
 
-function App() {
-
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
       <Footer />
     </div>
   );
 }
-
-export default App
