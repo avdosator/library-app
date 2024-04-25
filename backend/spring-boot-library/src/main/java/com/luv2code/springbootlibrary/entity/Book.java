a81dd0907faf0847b45b612a514fbdfbeb8b3237
Avdo Sator <avdo.sator@hotmail.com>
2024-04-25 20:11:26 +0200

    Add properties of Book class

--- backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -8,4 +8,29 @@ import javax.persistence.*;
 @Entity
 @Table(name = "book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private String copies;
+
+    @Column(name = "copies_available")
+    private String copies_available;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
 }
