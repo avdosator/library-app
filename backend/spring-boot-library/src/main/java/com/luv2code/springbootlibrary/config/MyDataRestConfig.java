e3e7a92f5f6efc18dcc4229ad51fc103127bc92d
Avdo Sator <avdo.sator@hotmail.com>
2024-04-25 22:00:21 +0200

    Create MyDataRestConfig class

---
+++ backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,8 @@
+package com.luv2code.springbootlibrary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+}
