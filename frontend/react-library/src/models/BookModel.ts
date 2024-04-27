69a9dfe481079a7d9612dce80e7a683edac1978e
Avdo Sator <avdo.sator@hotmail.com>
2024-04-27 10:54:38 +0200

    Add properties to BookModel

--- frontend/react-library/src/models/BookModel.ts
+++ frontend/react-library/src/models/BookModel.ts
@@ -1,4 +1,13 @@
 class BookModel {
+    id: number;
+    title: string;
+    author: string;
+    description: string;
+    copies: number;
+    copiesAvailable: number;
+    category: string;
+    img: string;
+
 
 }
 
