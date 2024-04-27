b753aee3adb66a8801139f1aba339f392997ba55
Avdo Sator <avdo.sator@hotmail.com>
2024-04-27 10:59:36 +0200

    Add constructor

--- frontend/react-library/src/models/BookModel.ts
+++ frontend/react-library/src/models/BookModel.ts
@@ -8,6 +8,18 @@ class BookModel {
     category: string;
     img: string;
 
+    public constructor(id: number, title: string, author: string, description: string,
+        copies: number, copiesAvailable: number, category: string, img: string) {
+
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.description = description;
+            this.copies = copies;
+            this.copiesAvailable = copiesAvailable;
+            this.category = category;
+            this.img = img;
+    }
 
 }
 
